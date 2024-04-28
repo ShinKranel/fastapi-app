8762ff4c037dc49dbaa8eeff22906a631bf7653f
Shin Kranel <shinkranel@gmail.com>
2024-04-28 14:11:36 +0100

    Update manager.py

--- auth/manager.py
+++ auth/manager.py
@@ -10,7 +10,7 @@ SECRET = SECRET_MANAGER
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    reset_password_ token_secret = SECRET
+    reset_password_token_secret = SECRET
     verification_token_secret = SECRET
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
