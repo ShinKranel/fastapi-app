8ca22567782b42c44d23fda9840e7d895f80f8c4
Shin Kranel <shinkranel@gmail.com>
2024-04-29 14:21:28 +0100

    Add operations/schemas.py, update oper../router.py

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,13 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database import get_async_session
+from src.operations.models import operation
+from src.operations.schemas import OperationCreate
 
 router = APIRouter()
 
 
 @router.get("/")
-async def get_operations():
-    return
+async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+    query = select(operation).where(operation.c.type == operation_type)
+    result = await session.execute(query)
+    return result.all()
 
 
-# ORM - Object-relational model
+@router.post("/")
+async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}
 
-#SQL Injection
+
+# query - запрос на выборку (select)
+# statement (stmt) - запрос на вставку и удаление
